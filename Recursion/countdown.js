f9280e1f01512df2ec4c0b897d04cec06678a1aa
Dhruv Arora <dhruvarora.code@gmail.com>
2023-03-12 11:04:06 +0530

    countdown problem solved

--- Recursion/countdown.js
+++ Recursion/countdown.js
@@ -1,14 +1,19 @@
 "use strict";
 
 const countDown = function (num) {
-  if (num <= 0) {
+  //condition
+  if (num <= 0) {  
     console.log("All Done!");
     return;
   }
+  //action
   console.log(num);
+
+  //different inputs
   num--;
+
+  //again called
   countDown(num);
 };
 
 countDown(5);
-
