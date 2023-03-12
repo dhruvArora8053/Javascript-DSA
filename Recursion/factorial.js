15a66a0d4b03e2fd85fd83c55aae9a9806e98175
Dhruv Arora <dhruvarora.code@gmail.com>
2023-03-12 11:50:25 +0530

    factorial algorithm implemented

---
+++ Recursion/factorial.js
@@ -0,0 +1,12 @@
+"use strict";
+
+//This function will return the factorial of a number using recursion:-
+const factorial = function (num) {
+    //base condition
+  if (num === 1) return 1;
+
+  //action+ different input+ again called
+  return num * factorial(num - 1);
+};
+
+console.log(factorial(5));
