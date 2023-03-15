f607145ca55cdda970013e231bc963da738b3140
Dhruv Arora <dhruvarora.code@gmail.com>
2023-03-15 11:52:46 +0530

    function recalled

--- Recursion/collectOddValues_helperMethodRecursion.js
+++ Recursion/collectOddValues_helperMethodRecursion.js
@@ -1,51 +1,28 @@
 "use strict";
 
-// //This function will return odd values from an array using helper method recursion:-
-// function collectOddValues(arr) {
-//   let result = [];
+//This function will return odd values from an array using helper method recursion:-
+function collectOddValues(arr) {
+  let result = [];
 
-//   //helper function
-//   function helper(helperInput) {
-//     //base condition
-//     if (helperInput.length === 0) {
-//       return;
-//     }
-
-//     //action
-//     if (helperInput[0] % 2 !== 0) {
-//       result.push(helperInput[0]);
-//     }
-
-//     //new input and called again
-//     helper(helperInput.slice(1));
-//   }
-
-//   helper(arr);
-
-//   return result;
-// }
-
-// console.log(collectOddValues([1, 2, 3, 4, 5, 6, 7, 8, 9]));
-
-const collectOddValues = function (arr) {
-  let newArr = [];
-
-  const helperMethod = function (helperInput) {
-    //condition
+  //helper function
+  function helper(helperInput) {
+    //base condition
     if (helperInput.length === 0) {
       return;
     }
 
     //action
     if (helperInput[0] % 2 !== 0) {
-      newArr.push(helperInput[0]);
+      result.push(helperInput[0]);
     }
 
-    helperMethod(helperInput.slice(1));
-  };
+    //new input and called again
+    helper(helperInput.slice(1));
+  }
 
-  helperMethod(arr);
-  return newArr;
-};
+  helper(arr);
+
+  return result;
+}
 
 console.log(collectOddValues([1, 2, 3, 4, 5, 6, 7, 8, 9]));
--- Recursion/collectOddValues_pureRecursion.js
+++ Recursion/collectOddValues_pureRecursion.js
@@ -1,22 +1,41 @@
 "use strict";
 
-//This function will return odd values from an array using pure recursion:-
+// //This function will return odd values from an array using pure recursion:-
+// const collectOddValues = function (arr) {
+//   let newArr = [];
+
+//   //base condition
+//   if (arr.length === 0) {
+//     return newArr;
+//   }
+
+//   //action
+//   if (arr[0] % 2 !== 0) {
+//     newArr.push(arr[0]);
+//   }
+
+//   //new input and called again
+//   newArr = newArr.concat(collectOddValues(arr.slice(1)));
+//   return newArr;
+// };
+
+// console.log(collectOddValues([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+
 const collectOddValues = function (arr) {
   let newArr = [];
 
-  //base condition
+  //condition
   if (arr.length === 0) {
     return newArr;
   }
 
-  //action
   if (arr[0] % 2 !== 0) {
     newArr.push(arr[0]);
   }
 
-  //new input and called again
+  //new input and call again
   newArr = newArr.concat(collectOddValues(arr.slice(1)));
   return newArr;
 };
 
-console.log(collectOddValues([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+console.log(collectOddValues([1, 2, 3, 4, 5]));
