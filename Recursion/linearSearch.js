91495390c041b0ec5e92a39cd18b6078946a9ef2
Dhruv Arora <dhruvarora.code@gmail.com>
2023-03-17 11:58:12 +0530

    linear search implemented

---
+++ Recursion/linearSearch.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const linearSearch = function (arr, value) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === value) {
+      return i;
+    }
+  }
+  return -1;
+};
+
+console.log(linearSearch([1, 2, 3, 4, 5, 6, 7, 8, 9], 8));
