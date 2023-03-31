ba83e3f0852fd1ca21caa499103c33947dd45b36
Dhruv Arora <dhruvarora.code@gmail.com>
2023-03-31 15:32:42 +0530

    pureRecursion implemented

--- Recursion/4-collectOddValues_pureRecursion.js
+++ Recursion/4-collectOddValues_pureRecursion.js
@@ -1,41 +1,29 @@
 "use strict";
 
-// //This function will return odd values from an array using pure recursion:-
-// const collectOddValues = function (arr) {
-//   let newArr = [];
-
-//   //base condition
-//   if (arr.length === 0) {
-//     return newArr;
-//   }
-
-//   //action
-//   if (arr[0] % 2 !== 0) {
-//     newArr.push(arr[0]);
-//   }
-
-//   //new input and called again
-//   newArr = newArr.concat(collectOddValues(arr.slice(1)));
-//   return newArr;
-// };
-
-// console.log(collectOddValues([1, 2, 3, 4, 5, 6, 7, 8, 9]));
-
+//This function will return odd values from an array using pure recursion:-
 const collectOddValues = function (arr) {
   let newArr = [];
 
-  //condition
+  //base condition
   if (arr.length === 0) {
     return newArr;
   }
 
+  //action
   if (arr[0] % 2 !== 0) {
     newArr.push(arr[0]);
   }
 
-  //new input and call again
+  //new input and called again
   newArr = newArr.concat(collectOddValues(arr.slice(1)));
   return newArr;
 };
 
 console.log(collectOddValues([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+
+//reference
+const a = [];
+const b = [1, 2];
+const c = [];
+const d = [3];
+console.log(a.concat(b).concat(c).concat(d));
