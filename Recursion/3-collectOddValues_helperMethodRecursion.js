1b0eec238972bb637c313aca0ef74daffad87ed8
Dhruv Arora <dhruvarora.code@gmail.com>
2023-04-14 10:54:37 +0530

    collecOddValues helper method implemented

--- Recursion/3-collectOddValues_helperMethodRecursion.js
+++ Recursion/3-collectOddValues_helperMethodRecursion.js
@@ -1,28 +1,50 @@
 "use strict";
 
 //This function will return odd values from an array using helper method recursion:-
-function collectOddValues(arr) {
-  let result = [];
+// function collectOddValues(arr) {
+//   let result = [];
 
-  //helper function
-  function helper(helperInput) {
+//   //helper function
+//   function helper(helperInput) {
+//     //base condition
+//     if (helperInput.length === 0) {
+//       return;
+//     }
+
+//     //action
+//     if (helperInput[0] % 2 !== 0) {
+//       result.push(helperInput[0]);
+//     }
+
+//     //new input and called again
+//     helper(helperInput.slice(1));
+//   }
+
+//   helper(arr);
+
+//   return result;
+// }
+
+// console.log(collectOddValues([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+
+const collectOddValues = function (arr) {
+  const newArr = [];
+
+  const helperMethod = function (helperArr) {
     //base condition
-    if (helperInput.length === 0) {
-      return;
-    }
+    if (helperArr.length === 0) return;
 
     //action
-    if (helperInput[0] % 2 !== 0) {
-      result.push(helperInput[0]);
+    if (helperArr[0] % 2 !== 0) {
+      newArr.push(helperArr[0]);
     }
 
-    //new input and called again
-    helper(helperInput.slice(1));
-  }
+    //different input and call again
+    helperMethod(helperArr.slice(1));
+  };
 
-  helper(arr);
-
-  return result;
-}
+  helperMethod(arr);
+  return newArr;
+};
 
 console.log(collectOddValues([1, 2, 3, 4, 5, 6, 7, 8, 9]));
