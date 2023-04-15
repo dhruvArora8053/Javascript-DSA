fd6ceecd0726b3f6c1c30f2028413de51fd5cb4e
Dhruv Arora <dhruvarora.code@gmail.com>
2023-04-15 15:05:29 +0530

    palindrome implemented

---
+++ 03-Recursion Challenges/02-isPalindrome.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const palindrome = function (str) {
+  if (str.length === 1) {
+    return -1;
+  }
+
+  const con = str.toLowerCase().split("");
+  const secondPart = con.splice(Math.floor(con.length / 2, con.length - 1));
+
+  let com;
+  if (str.length % 2 !== 0) {
+    com = con.concat(secondPart.slice(0, 1));
+
+    if (com.join("") === secondPart.reverse().join("")) {
+      console.log("it is a palindrome");
+    }
+  } else if (str.length % 2 === 0) {
+    if (con.join("") === secondPart.reverse().join("")) {
+      console.log("it is a palindrome");
+    } else {
+      console.log("it is not a palindrome");
+    }
+  }
+};
+
