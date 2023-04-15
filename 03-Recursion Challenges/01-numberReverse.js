a163eb8407c9f279b15a102be901412c3312d911
Dhruv Arora <dhruvarora.code@gmail.com>
2023-04-15 14:33:37 +0530

    reverse implemented

--- 03-Recursion Challenges/01-numberReverse.js
+++ 03-Recursion Challenges/01-numberReverse.js
@@ -1,2 +1,9 @@
-'use strict'
+"use strict";
 
+//Write a program to reverse a given number using recursion:
+const reverse = function (n) {
+  const str = +String(n).split("").reverse().join("");
+  console.log(str);
+};
+
+reverse(123456);
