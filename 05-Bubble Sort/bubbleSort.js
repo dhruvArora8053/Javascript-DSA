a95028a6c752378fca6cee3a950c41e619728072
Dhruv Arora <dhruvarora.code@gmail.com>
2023-05-02 00:22:27 +0530

    bubble sort implemented

--- 05-Bubble Sort/bubbleSort.js
+++ 05-Bubble Sort/bubbleSort.js
@@ -1,1 +1,18 @@
 "use strict";
+
+const bubbleSort = function (arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      //   console.log(arr, arr[j], arr[j + 1]);
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+};
+
+console.log(bubbleSort([37, 45, 29, 8]));
+console.log(bubbleSort([1, 37, 44, 21, 4, 3, 15, 7]));
