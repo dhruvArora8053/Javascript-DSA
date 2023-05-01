c4650aa6d55191be2e3d049e623fc41c88c824fa
Dhruv Arora <dhruvarora.code@gmail.com>
2023-05-01 23:35:57 +0530

    new file added

--- 04-Searching Algorithms/3-naiveStringSearch.js
+++ 04-Searching Algorithms/3-naiveStringSearch.js
@@ -2,13 +2,13 @@
 
 //Write a program to find number of string patterns inside a string and return counted patterns:
 
-const naiveStringSearch = function (string, pattern) {
+const naiveStringSearch = function (str, pattern) {
   let count = 0;
   let a = 0;
-  let b = 3;
+  let b = pattern.length;
 
-  while (b <= string.length) {
-    if (string.slice(a, b) === pattern) {
+  while (b <= str.length) {
+    if (str.slice(a, b) === pattern) {
       count++;
     }
     a++;
@@ -19,5 +19,9 @@ const naiveStringSearch = function (string, pattern) {
 
 console.log(naiveStringSearch("womgwomgzomgomg", "omg"));
 console.log(naiveStringSearch("lsdafjdflsakaakaakadfsakadflaka", "aka"));
+console.log(naiveStringSearch("lorie loled", "lol"));
+console.log(naiveStringSearch("lorie loled", "lo"));
+console.log(naiveStringSearch("lorie loled", "pop"));
 
 // console.log('lsdfk'.slice(0,3));
+
