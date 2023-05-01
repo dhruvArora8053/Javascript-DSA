e7e78580de0fd4ce81cd3363a4e5bca9e9d9af3c
Dhruv Arora <dhruvarora.code@gmail.com>
2023-05-02 01:08:59 +0530

    bubble sort optimized

--- 05-Bubble Sort/bubbleSort.js
+++ 05-Bubble Sort/bubbleSort.js
@@ -1,14 +1,29 @@
 "use strict";
 
+const swap = function (arr, a, b) {
+  return ([arr[a], arr[b]] = [arr[b], arr[a]]);
+};
+
 const bubbleSort = function (arr) {
   for (let i = 0; i < arr.length - 1; i++) {
+    let noSwap = true;
     for (let j = 0; j < arr.length - i - 1; j++) {
       //   console.log(arr, arr[j], arr[j + 1]);
       if (arr[j] > arr[j + 1]) {
+        //traditional
         let temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
+
+        //modern
+        // swap(arr, j, j + 1);
+
+        //optimization
+        noSwap = false;
       }
+    }
+    if (noSwap) {
+      break;
     }
   }
   return arr;
@@ -16,6 +31,7 @@ const bubbleSort = function (arr) {
 
 console.log(bubbleSort([37, 45, 29, 8]));
 console.log(bubbleSort([1, 37, 44, 21, 4, 3, 15, 7]));
+console.log(bubbleSort([8, 1, 2, 3, 4, 5, 6, 7]));
 
 //another loops could be
 // for(let i = arr.length; i > 0; i--)
