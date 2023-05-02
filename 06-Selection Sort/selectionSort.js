610a395cfb3b4ebbee4b246e750458c5d9c0d1c2
Dhruv Arora <dhruvarora.code@gmail.com>
2023-05-02 15:56:33 +0530

    big o added

--- 06-Selection Sort/selectionSort.js
+++ 06-Selection Sort/selectionSort.js
@@ -4,13 +4,17 @@ const selectionSort = function (arr) {
   for (let i = 0; i < arr.length - 1; i++) {
     let min = i;
     for (let j = i + 1; j < arr.length; j++) {
+      //   console.log(i, j);
       if (arr[min] > arr[j]) {
         min = j;
       }
     }
-    let temp = arr[i];
-    arr[i] = arr[min];
-    arr[min] = temp;
+    //optimization
+    if (min !== i) {
+      let temp = arr[i];
+      arr[i] = arr[min];
+      arr[min] = temp;
+    }
   }
   return arr;
 };
@@ -18,3 +22,6 @@ const selectionSort = function (arr) {
 console.log(selectionSort([37, 45, 29, 8]));
 console.log(selectionSort([1, 37, 44, 21, 4, 3, 15, 7]));
 console.log(selectionSort([8, 1, 2, 3, 4, 5, 6, 7]));
+console.log(selectionSort([-8, 1, 2, -3, 4, -5, 6, 7]));
+
+//Big O: n**2
