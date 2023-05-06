2a6ca2ecbe1e2775b1606587581a2225b126fad5
Dhruv Arora <dhruvarora.code@gmail.com>
2023-05-06 21:44:57 +0530

    selection sort implemented

--- 06-Selection Sort/selectionSort.js
+++ 06-Selection Sort/selectionSort.js
@@ -24,25 +24,6 @@
 // console.log(selectionSort([8, 1, 2, 3, 4, 5, 6, 7]));
 // console.log(selectionSort([-8, 1, 2, -3, 4, -5, 6, 7]));
 
-//Big O: n**2
-
-const selectionSort = function (arr) {
-  for (let i = 0; i < arr.length - 1; i++) {
-    let min = i;
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[min] > arr[j]) {
-        min = j;
-      }
-    }
-    const temp = arr[i];
-    arr[i] = arr[min];
-    arr[min] = temp;
-  }
-  return arr;
-};
-
-console.log(selectionSort([37, 45, 29, 8]));
-console.log(selectionSort([1, 37, 44, 21, 4, 3, 15, 7]));
-console.log(selectionSort([8, 1, 2, 3, 4, 5, 6, 7]));
-console.log(selectionSort([-8, 1, 2, -3, 4, -5, 6, 7]));
+//Big O: best - n**2 || worst - n**2
+//space complexity - O(1)
 
