50d44db4567f07357d4a9748bcc27faea1415932
Dhruv Arora <dhruvarora.code@gmail.com>
2023-05-06 21:37:46 +0530

    selection sort implemented

--- 05-Bubble Sort/bubbleSort.js
+++ 05-Bubble Sort/bubbleSort.js
@@ -4,57 +4,21 @@
 //   return ([arr[a], arr[b]] = [arr[b], arr[a]]);
 // };
 
-// const bubbleSort = function (arr) {
-//   for (let i = 0; i < arr.length - 1; i++) {
-//     let noSwap = true;
-//     for (let j = 0; j < arr.length - i - 1; j++) {
-//       //   console.log(arr, arr[j], arr[j + 1]);
-//       if (arr[j] > arr[j + 1]) {
-//         //traditional
-//         let temp = arr[j];
-//         arr[j] = arr[j + 1];
-//         arr[j + 1] = temp;
-
-//         //modern
-//         // swap(arr, j, j + 1);
-
-//         //optimization
-//         noSwap = false;
-//       }
-//     }
-//     if (noSwap) {
-//       break;
-//     }
-//   }
-//   return arr;
-// };
-
-// console.log(bubbleSort([37, 45, 29, 8]));
-// console.log(bubbleSort([1, 37, 44, 21, 4, 3, 15, 7]));
-// console.log(bubbleSort([8, 1, 2, 3, 4, 5, 6, 7]));
-
-//another loops could be
-// for(let i = arr.length; i > 0; i--)
-// for(let j = 0; j < i-1; j++)
-
-//Big O:- Worst: n**2, best: n
-
-//Use Cases:-
-//When your data is nearly sorted then it could be a good candidate
-const swap = function (arr, a, b) {
-  return ([arr[a], arr[b]] = [arr[b], arr[a]]);
-};
-
 const bubbleSort = function (arr) {
   for (let i = 0; i < arr.length - 1; i++) {
     let noSwap = true;
-    for (let j = 0; j < arr.length - 1 - i; j++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      //   console.log(arr, arr[j], arr[j + 1]);
       if (arr[j] > arr[j + 1]) {
-        // const temp = arr[j];
-        // arr[j] = arr[j + 1];
-        // arr[j + 1] = temp;
-        swap(arr, j, j + 1);
+        //traditional
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
 
+        //modern
+        // swap(arr, j, j + 1);
+
+        //optimization
         noSwap = false;
       }
     }
@@ -68,4 +32,13 @@ const bubbleSort = function (arr) {
 console.log(bubbleSort([37, 45, 29, 8]));
 console.log(bubbleSort([1, 37, 44, 21, 4, 3, 15, 7]));
 console.log(bubbleSort([8, 1, 2, 3, 4, 5, 6, 7]));
-console.log(bubbleSort([1, 2, 3, 4, 5, 6, 7, 8]));
+console.log(bubbleSort([1, 2, 3, 4, 5, 6, 7]));
+
+//another loops could be
+// for(let i = arr.length; i > 0; i--)
+// for(let j = 0; j < i-1; j++)
+
+//Big O:- Worst: n**2, best: n
+
+//Use Cases:-
+//When your data is nearly sorted then it could be a good candidate
--- 06-Selection Sort/selectionSort.js
+++ 06-Selection Sort/selectionSort.js
@@ -1,20 +1,42 @@
 "use strict";
 
+// const selectionSort = function (arr) {
+//   for (let i = 0; i < arr.length - 1; i++) {
+//     let min = i;
+//     for (let j = i + 1; j < arr.length; j++) {
+//       //   console.log(i, j);
+//       if (arr[min] > arr[j]) {
+//         min = j;
+//       }
+//     }
+//     //optimization
+//     if (min !== i) {
+//       let temp = arr[i];
+//       arr[i] = arr[min];
+//       arr[min] = temp;
+//     }
+//   }
+//   return arr;
+// };
+
+// console.log(selectionSort([37, 45, 29, 8]));
+// console.log(selectionSort([1, 37, 44, 21, 4, 3, 15, 7]));
+// console.log(selectionSort([8, 1, 2, 3, 4, 5, 6, 7]));
+// console.log(selectionSort([-8, 1, 2, -3, 4, -5, 6, 7]));
+
+//Big O: n**2
+
 const selectionSort = function (arr) {
   for (let i = 0; i < arr.length - 1; i++) {
     let min = i;
     for (let j = i + 1; j < arr.length; j++) {
-      //   console.log(i, j);
       if (arr[min] > arr[j]) {
         min = j;
       }
     }
-    //optimization
-    if (min !== i) {
-      let temp = arr[i];
-      arr[i] = arr[min];
-      arr[min] = temp;
-    }
+    const temp = arr[i];
+    arr[i] = arr[min];
+    arr[min] = temp;
   }
   return arr;
 };
@@ -24,4 +46,3 @@ console.log(selectionSort([1, 37, 44, 21, 4, 3, 15, 7]));
 console.log(selectionSort([8, 1, 2, 3, 4, 5, 6, 7]));
 console.log(selectionSort([-8, 1, 2, -3, 4, -5, 6, 7]));
 
-//Big O: n**2
