4ecf0b77841b4d81a24878535f646ecd94d35f76
Dhruv Arora <dhruvarora.code@gmail.com>
2023-05-07 17:25:14 +0530

    added complexity

--- 07-Insertion Sort/insertionSort.js
+++ 07-Insertion Sort/insertionSort.js
@@ -18,3 +18,5 @@ console.log(insertionSort([37, 45, 29, 8]));
 console.log(insertionSort([1, 37, 44, 21, 4, 3, 15, 7]));
 console.log(insertionSort([8, 1, 2, 3, 4, 5, 6, 7]));
 console.log(insertionSort([-8, 1, 2, -3, 4, -5, 6, 7]));
+
+//Big O: best case: n || worst case: n**2
