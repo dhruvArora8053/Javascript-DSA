6f9edb62e9f536943d7f99b027a02bb8cf83aa84
Dhruv Arora <dhruvarora.code@gmail.com>
2023-05-08 12:50:53 +0530

    implemented insertion sort

--- 06-Selection Sort/selectionSort.js
+++ 06-Selection Sort/selectionSort.js
@@ -27,22 +27,4 @@ console.log(selectionSort([-8, 1, 2, -3, 4, -5, 6, 7]));
 //Big O: best - n**2 || worst - n**2
 //space complexity - O(1)
 
-const selectionSort1 = function (arr) {
-  for (let i = 0; i < arr.length - 1; i++) {
-    let min = i;
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[min] > arr[j]) {
-        min = j;
-      }
-    }
-    let temp = arr[i];
-    arr[i] = arr[min];
-    arr[min] = temp;
-  }
-  return arr;
-};
 
-console.log(selectionSort1([37, 45, 29, 8]));
-console.log(selectionSort1([1, 37, 44, 21, 4, 3, 15, 7]));
-console.log(selectionSort1([8, 1, 2, 3, 4, 5, 6, 7]));
-console.log(selectionSort1([-8, 1, 2, -3, 4, -5, 6, 7]));
