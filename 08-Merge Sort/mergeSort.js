dd27b127e1f9be73fc7e5d07c73877389398c74b
Dhruv Arora <dhruvarora.code@gmail.com>
2023-05-09 10:37:21 +0530

    implementing merging

--- 08-Merge Sort/mergeSort.js
+++ 08-Merge Sort/mergeSort.js
@@ -1,1 +1,35 @@
 "use strict";
+
+const mergeSort = function (arr1, arr2) {
+  let newArr = [];
+  let i,
+    j = 0;
+  while (i < arr1.length && j < arr2.length) {
+    while (arr1[i] > arr2[j]) {
+      newArr.push(arr2[j]);
+      j++;
+    }
+
+    if (arr1[i] < arr2[j]) {
+      newArr.push(arr1[i]);
+    }
+
+    i++;
+  }
+
+  if (i === arr1.length) {
+    for (let a = j; a < arr2.length; a++) {
+      newArr.push(arr2[a]);
+    }
+  }
+
+  if (j === arr2.length) {
+    for (let b = i; b < arr1.length; b++) {
+      newArr.push(arr1[b]);
+    }
+  }
+
+  return newArr;
+};
+
+console.log(mergeSort([1, 10, 50], [2, 14, 99, 100]));
