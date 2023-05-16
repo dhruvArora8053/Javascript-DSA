f3f74bc4a3c0483c5c8629cb6e10034a9de39593
Dhruv Arora <dhruvarora.code@gmail.com>
2023-05-16 10:35:42 +0530

    insertion sort implemented

--- 07-Insertion Sort/insertionSort.js
+++ 07-Insertion Sort/insertionSort.js
@@ -22,4 +22,21 @@ console.log(insertionSort([-8, 1, 2, -3, 4, -5, 6, 7]));
 //Big O: best case: n || worst case: n**2
 //space complexity: O(1)
 
+const insertionSort1 = function (arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let currentVal = arr[i];
+    for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+      arr[j + 1] = arr[j];
+    }
+    arr[j + 1] = currentVal;
+  }
 
+  return arr;
+};
+
+console.log(insertionSort1([2, 1, 9, 76, 4]));
+console.log(insertionSort1([42, 33, 11]));
+console.log(insertionSort1([37, 45, 29, 8]));
+console.log(insertionSort1([1, 37, 44, 21, 4, 3, 15, 7]));
+console.log(insertionSort1([8, 1, 2, 3, 4, 5, 6, 7]));
+console.log(insertionSort1([-8, 1, 2, -3, 4, -5, 6, 7]));
