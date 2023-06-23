f1dc9d5ef8b2e0bb2a1bbf39dd5150cd50b5ca7f
Dhruv Arora <dhruvarora.code@gmail.com>
2023-06-23 13:38:46 +0530

    bubble sort implemented

--- 05-Bubble Sort/bubbleSort.js
+++ 05-Bubble Sort/bubbleSort.js
@@ -33,6 +33,7 @@ console.log(bubbleSort([37, 45, 29, 8]));
 console.log(bubbleSort([1, 37, 44, 21, 4, 3, 15, 7]));
 console.log(bubbleSort([8, 1, 2, 3, 4, 5, 6, 7]));
 console.log(bubbleSort([1, 2, 3, 4, 5, 6, 7]));
+console.log(bubbleSort([-8, 1, 2, -3, 4, -5, 6, 7]));
 
 //another loops could be
 // for(let i = arr.length; i > 0; i--)
@@ -43,26 +44,24 @@ console.log(bubbleSort([1, 2, 3, 4, 5, 6, 7]));
 //Use Cases:-
 //When your data is nearly sorted then it could be a good candidate
 
-//My Bubble Sort:-
-
-const swap = function (arr, a, b) {
+const swap1 = function (arr, a, b) {
   return ([arr[a], arr[b]] = [arr[b], arr[a]]);
 };
 
 const bubbleSort1 = function (arr) {
   for (let i = 0; i < arr.length - 1; i++) {
-    let noswap = true;
+    let swap = false;
     for (let j = 0; j < arr.length - 1 - i; j++) {
       if (arr[j] > arr[j + 1]) {
-        // const temp = arr[j];
+        // let temp = arr[j];
         // arr[j] = arr[j + 1];
         // arr[j + 1] = temp;
-        swap(arr, j, j + 1);
-        noswap = false;
+        swap1(arr, j, j + 1);
+        swap = true;
       }
-    }
-    if (noswap) {
-      break;
+      if (!swap) {
+        break;
+      }
     }
   }
   return arr;
@@ -72,4 +71,4 @@ console.log(bubbleSort1([37, 45, 29, 8]));
 console.log(bubbleSort1([1, 37, 44, 21, 4, 3, 15, 7]));
 console.log(bubbleSort1([8, 1, 2, 3, 4, 5, 6, 7]));
 console.log(bubbleSort1([1, 2, 3, 4, 5, 6, 7]));
-
+console.log(bubbleSort1([-8, 1, 2, -3, 4, -5, 6, 7]));
