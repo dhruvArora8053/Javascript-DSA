8492b9f3cf428beea85fa84b370a4aff27745903
Dhruv Arora <dhruvarora.code@gmail.com>
2023-06-23 23:13:49 +0530

    algorithms implemented

---
+++ 10-Radix Sort/radixSort.js
@@ -0,0 +1,44 @@
+const getDigit = function (num, pos) {
+  return +String(Math.abs(num))
+    .split("")
+    .reverse()
+    .slice(pos, pos + 1)
+    .join("");
+};
+
+console.log(getDigit(12345, 0));
+console.log(getDigit(12345, 1));
+console.log(getDigit(12345, 2));
+console.log(getDigit(12345, 3));
+console.log(getDigit(12345, 4));
+console.log(getDigit(12345, 5));
+console.log("-----------------------------");
+console.log(getDigit(7323, 0));
+console.log(getDigit(7323, 1));
+console.log(getDigit(7323, 2));
+console.log(getDigit(7323, 3));
+console.log("-----------------------------");
+
+const digitCount = function (num) {
+  return String(Math.abs(num)).split("").length;
+};
+
+console.log(digitCount(1));
+console.log(digitCount(25));
+console.log(digitCount(314));
+console.log('--------------------------');
+
+const mostDigits = function (arr) {
+  let max = digitCount(arr[0]);
+
+  for (let i = 1; i < arr.length; i++) {
+    if (max < digitCount(arr[i])) {
+      max = digitCount(arr[i]);
+    }
+  }
+  return max;
+};
+
+console.log(mostDigits([1234, 56, 7]));
+console.log(mostDigits([1, 1, 1111, 1]));
+console.log(mostDigits([12, 34, 56, 78]));
