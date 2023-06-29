c504c231101f4e4d0bf76b52fddd6336c0a7be4f
Dhruv Arora <dhruvarora.code@gmail.com>
2023-06-29 10:56:47 +0530

    linear search implemented

--- 04-Searching Algorithms/1-linearSearch.js
+++ 04-Searching Algorithms/1-linearSearch.js
@@ -11,3 +11,13 @@ const linearSearch = function (arr, value) {
 
 console.log(linearSearch([1, 2, 3, 4, 5, 6, 7, 8, 9], 8));
 
+const linearSearch1 = function (arr, item) {
+  for (let i = 0; i < arr.length; i++)
+    if (arr[i] === item) {
+      return i;
+    }
+
+  return -1;
+};
+
+console.log(linearSearch1([1, 2, 3, 4, 5, 6, 7, 8, 9], 4));
