5901cf2bc9458e09751f67c9d311e2e9ac237f0e
Dhruv Arora <dhruvarora.code@gmail.com>
2023-06-29 10:36:59 +0530

    factorial implemented

--- 02-Recursion/2-factorial.js
+++ 02-Recursion/2-factorial.js
@@ -11,4 +11,13 @@ const factorial = function (num) {
 
 console.log(factorial(1));
 
+const factorial1 = function (num) {
+  //base condition
+  if (num === 1) {
+    return 1;
+  }
 
+  return num * factorial(num - 1);
+};
+
+console.log(factorial1(5));
