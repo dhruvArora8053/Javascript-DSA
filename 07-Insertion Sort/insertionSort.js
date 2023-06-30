2719163b55f4adbde08c927d6489d38e64be70dc
Dhruv Arora <dhruvarora.code@gmail.com>
2023-06-30 10:16:01 +0530

    insertion sort implemented

--- 07-Insertion Sort/insertionSort.js
+++ 07-Insertion Sort/insertionSort.js
@@ -24,7 +24,16 @@ console.log(insertionSort([-8, 1, 2, -3, 4, -5, 6, 7]));
 
 //Bubble and insertion sort do well in nearly sorted arrays than other alogrithms.
 
-
+const insertionSort1 = function (arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let currentVal = arr[i];
+    for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+      arr[j + 1] = arr[j];
+    }
+    arr[j + 1] = currentVal;
+  }
+  return arr;
+};
 
 console.log(insertionSort1([2, 1, 9, 76, 4]));
 console.log(insertionSort1([42, 33, 11]));
