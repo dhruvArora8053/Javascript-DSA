d8053223544999ccefd93822db61ad7d1f8427f2
Dhruv Arora <dhruvarora.code@gmail.com>
2023-07-01 22:23:02 +0530

    push implemented

--- 12-linkedList.js/linkedList.js
+++ 12-linkedList.js/linkedList.js
@@ -1,6 +1,6 @@
 "use strict";
 
-//piece of data --> val
+// piece of data --> val
 // reference to next node --> node
 
 class Node {
@@ -10,8 +10,38 @@ class Node {
   }
 }
 
-let first = new Node("HI");
-first.next = new Node("there");
-first.next.next = new Node("how");
-first.next.next.next = new Node("are");
-first.next.next.next.next = new Node("you");
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length += 1;
+    return this;
+  }
+}
+
+const list = new SinglyLinkedList();
+list.push("Hello");
+console.log(list);
+
+list.push("Goodbye");
+list.push(99);
+console.log(list.head.next.next);
+
+// let first = new Node("HI");
+// first.next = new Node("there");
+// first.next.next = new Node("how");
+// first.next.next.next = new Node("are");
+// first.next.next.next.next = new Node("you");
+// console.log(first);
