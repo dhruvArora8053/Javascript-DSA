86780e90c9ddd063fe7326d0d581a8f2431b3269
Dhruv Arora <dhruvarora.code@gmail.com>
2023-07-01 21:04:27 +0530

    basic linked list implemented

---
+++ 12-linkedList.js/linkedList.js
@@ -0,0 +1,17 @@
+"use strict";
+
+//piece of data --> val
+// reference to next node --> node
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+let first = new Node("HI");
+first.next = new Node("there");
+first.next.next = new Node("how");
+first.next.next.next = new Node("are");
+first.next.next.next.next = new Node("you");
