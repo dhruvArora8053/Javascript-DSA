a0d262d9f41a0f1a162b9750ebb1422e1ef6760c
Dhruv Arora <dhruvarora.code@gmail.com>
2023-07-02 12:57:26 +0530

    implemented pivot

--- 09-Quick Sort/quickSort.js
+++ 09-Quick Sort/quickSort.js
@@ -6,7 +6,7 @@ const swap = function (array, i, j) {
   array[j] = temp;
 };
 
-const pivot = function (arr, start = 0, end = arr.lenght - 1) {
+const pivot = function (arr, start = 0, end = arr.length - 1) {
   var pivot = arr[start];
   var swapIdx = start;
 
@@ -28,3 +28,29 @@ console.log(pivot([8, 1, 2, 3, 4, 5, 6, 7]));
 console.log(pivot([-8, 1, 2, -3, 4, -5, 6, 7]));
 console.log(pivot([23, 2, 1, 9, 17, 345, 8, 12, 14, 5476, 2345, 9852]));
 console.log(pivot([-23, -333, -400, 12, 1, 2, 3, 44, -233]));
+console.log("--------------------------------");
+
+const pivot1 = function (arr, start = 0, end = arr.length) {
+  let pivot = arr[start];
+  let pivotIndex = start;
+
+  for (let i = 1; i < arr.length; i++) {
+    if (pivot > arr[i]) {
+      pivotIndex++;
+      swap(pivotIndex, i);
+    }
+  }
+
+  swap(start, pivotIndex);
+  return pivotIndex;
+};
+
+console.log(pivot([9, 2, 1, 76, 4]));
+console.log(pivot([42, 33, 11]));
+console.log(pivot([37, 45, 29, 8]));
+console.log(pivot([1, 37, 44, 21, 4, 3, 15, 7]));
+console.log(pivot([8, 1, 2, 3, 4, 5, 6, 7]));
+console.log(pivot([-8, 1, 2, -3, 4, -5, 6, 7]));
+console.log(pivot([23, 2, 1, 9, 17, 345, 8, 12, 14, 5476, 2345, 9852]));
+console.log(pivot([-23, -333, -400, 12, 1, 2, 3, 44, -233]));
+console.log("--------------------------------");
