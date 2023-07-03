b3f9650a0dc4fedf1e1d9e5ea07feb634edc591d
Dhruv Arora <dhruvarora.code@gmail.com>
2023-07-03 14:02:20 +0530

    push implemented

---
+++ 12-Doubly Linked List/01-push.js
@@ -0,0 +1,43 @@
+"use strict";
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    const newNode = new Node(val);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length += 1;
+    return this;
+  }
+}
+
+const list = new DoublyLinkedList();
+list.push(12);
+console.log(list);
+
+list.push(24);
+console.log(list);
+
+list.push(36);
+list.push(48);
+console.log(list);
