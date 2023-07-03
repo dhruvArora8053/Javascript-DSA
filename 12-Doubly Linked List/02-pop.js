5a956936786b15f857e3f15cbdfbedafad6b7951
Dhruv Arora <dhruvarora.code@gmail.com>
2023-07-03 14:28:17 +0530

    pop implemented

---
+++ 12-Doubly Linked List/02-pop.js
@@ -0,0 +1,62 @@
+"use strict";
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    const newNode = new Node(val);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length += 1;
+    return this;
+  }
+
+  pop() {
+    if (!this.head) {
+      return undefined;
+    }
+
+    let removed = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = this.tail.prev;
+      this.tail.next = null;
+    }
+
+    this.length -= 1;
+    removed.prev = null;
+    return removed;
+  }
+}
+
+const list = new DoublyLinkedList();
+list.push(12).push(24).push(36).push(48);
+console.log(list);
+
+console.log(list.pop());
+console.log(list.pop());
+console.log(list.pop());
+console.log(list.pop());
+console.log(list.pop());
+console.log(list);
