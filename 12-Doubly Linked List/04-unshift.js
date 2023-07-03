22ea24cd94b045a8c62120c0741ec9ed44060bdd
Dhruv Arora <dhruvarora.code@gmail.com>
2023-07-03 14:57:18 +0530

    unshift implemented

---
+++ 12-Doubly Linked List/04-unshift.js
@@ -0,0 +1,95 @@
+"use strict";
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    const newNode = new Node(val);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length += 1;
+    return this;
+  }
+
+  pop() {
+    if (!this.head) {
+      return undefined;
+    }
+
+    let removed = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = removed.prev;
+      this.tail.next = null;
+    }
+
+    this.length -= 1;
+    removed.prev = null;
+    return removed;
+  }
+
+  shift() {
+    if (!this.head) {
+      return undefined;
+    }
+
+    let removed = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = removed.next;
+      this.head.prev = null;
+      removed.next = null;
+    }
+    this.length -= 1;
+    return removed;
+  }
+
+  unshift(val) {
+    const newNode = new Node(val);
+
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.head.prev = newNode;
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length += 1;
+    return this;
+  }
+}
+
+const list = new DoublyLinkedList();
+list.push(12).push(24).push(36).push(48);
+console.log(list);
+
+list.unshift(1);
+list.unshift(2);
+// list.unshift(3);
+// list.unshift(4);
+// list.unshift(5);
+console.log(list);
