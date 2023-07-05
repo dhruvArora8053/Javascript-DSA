3fa9f649939c9ac4432922d42d544cdf25aedfd0
Dhruv Arora <dhruvarora.code@gmail.com>
2023-07-05 21:57:37 +0530

    extrackt max implemented

--- 16-Heaps/02-maxBinaryHeap_extractMax.js
+++ 16-Heaps/02-maxBinaryHeap_extractMax.js
@@ -21,7 +21,49 @@ class MaxBinaryHeap {
   }
 
   //remove or extract max:
-  extractMax() {}
+  extractMax() {
+    const max = this.values[0];
+    const end = this.values.pop();
+    this.values[0] = end;
+
+    //bubble down
+    this.sinkDown();
+    return max;
+  }
+
+  sinkDown() {
+    let index = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+        if (leftChild > element) {
+          swap = leftChildIdx;
+        }
+      }
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+        if (
+          (swap === null && rightChild > element) ||
+          (swap !== null && rightChild > leftChild)
+        ) {
+          swap = rightChildIdx;
+        }
+      }
+
+      if (swap === null) break;
+      this.values[index] = this.values[swap];
+      this.values[swap] = element;
+      index = swap;
+    }
+  }
 }
 
 const heap = new MaxBinaryHeap();
