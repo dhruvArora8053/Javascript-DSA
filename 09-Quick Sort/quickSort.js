71e74238a1d254c92de3f9a9dfa4ffaecc950df1
Dhruv Arora <dhruvarora.code@gmail.com>
2023-07-06 20:14:32 +0530

    quick sort implemented

--- 09-Quick Sort/quickSort.js
+++ 09-Quick Sort/quickSort.js
@@ -34,7 +34,7 @@ const pivot1 = function (arr, start = 0, end = arr.length - 1) {
   const pivot = arr[start];
   let pivotIndex = start;
 
-  for (let i = 1; i < arr.length; i++) {
+  for (let i = 1; i < end; i++) {
     if (pivot > arr[i]) {
       pivotIndex++;
       swap(arr, i, pivotIndex);
@@ -54,3 +54,25 @@ console.log(pivot1([23, 2, 1, 9, 17, 345, 8, 12, 14, 5476, 2345, 9852]));
 console.log(pivot1([-23, -333, -400, 12, 1, 2, 3, 44, -233]));
 console.log(pivot1([23, 2, 1, 9, 17, 345, 8, 12, 14, 5476, 2345, 9852]));
 console.log("--------------------------------");
+
+const quickSort = function (arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let pivotIndex = pivot(arr, left, right);
+
+    //left
+    quickSort(arr, left, pivotIndex - 1);
+    //right
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr;
+};
+
+console.log(quickSort([9, 2, 1, 76, 4]));
+console.log(quickSort([42, 33, 11]));
+console.log(quickSort([37, 45, 29, 8]));
+console.log(quickSort([1, 37, 44, 21, 4, 3, 15, 7]));
+console.log(quickSort([8, 1, 2, 3, 4, 5, 6, 7]));
+console.log(quickSort([-8, 1, 2, -3, 4, -5, 6, 7]));
+console.log(quickSort([23, 2, 1, 9, 17, 345, 8, 12, 14, 5476, 2345, 9852]));
+console.log(quickSort([-23, -333, -400, 12, 1, 2, 3, 44, -233]));
+console.log(quickSort([23, 2, 1, 9, 17, 345, 8, 12, 14, 5476, 2345, 9852]));
