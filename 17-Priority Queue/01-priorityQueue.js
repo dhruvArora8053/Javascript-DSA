0a2e0ddb404c7f6ed9b1c99c40ef5ece71987c7e
Dhruv Arora <dhruvarora.code@gmail.com>
2023-07-07 22:36:29 +0530

    hash map implemented

---
+++ 17-Priority Queue/01-priorityQueue.js
@@ -0,0 +1,127 @@
+"use strict";
+
+class Node {
+  constructor(val, priority) {
+    this.val = val;
+    this.priority = priority;
+  }
+}
+
+class MaxBinaryHeap {
+  constructor() {
+    this.values = [];
+  }
+
+  insert(val) {
+    this.values.push(val);
+
+    let index = this.values.length - 1;
+    let parentIndex = Math.floor(index - 1 / 2);
+    while (this.values[parentIndex] < this.values[index]) {
+      let temp = this.values[parentIndex];
+      this.values[parentIndex] = this.values[index];
+      this.values[index] = temp;
+      index = parentIndex;
+      parentIndex = Math.floor(index / 2);
+    }
+    return this;
+  }
+
+  //remove or extract max:
+  extractMax() {
+    const max = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return max;
+  }
+
+  sinkDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+        if (leftChild > element) {
+          swap = leftChildIdx;
+        }
+      }
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+        if (
+          (swap === null && rightChild > element) ||
+          (swap !== null && rightChild > leftChild)
+        ) {
+          swap = rightChildIdx;
+        }
+      }
+
+      if (swap === null) break;
+      let temp = this.values[idx];
+      this.values[idx] = this.values[swap];
+      this.values[swap] = temp;
+      idx = swap;
+    }
+  }
+
+  enqueue(val, priority) {
+    let newNode = new Node(val, priority);
+    this.values.push(newNode);
+
+    
+    
+  }
+}
+
+const heap = new MaxBinaryHeap();
+console.log(heap);
+
+// heap
+//   .insert(4)
+//   .insert(13)
+//   .insert(8)
+//   .insert(1)
+//   .insert(25)
+//   .insert(5)
+//   .insert(17)
+//   .insert(100)
+//   .insert(36)
+//   .insert(19)
+//   .insert(9)
+//   .insert(6)
+//   .insert(11)
+//   .insert(12)
+//   .insert(15);
+
+heap
+  .insert(41)
+  .insert(39)
+  .insert(33)
+  .insert(18)
+  .insert(27)
+  .insert(12)
+  .insert(55)
+  .insert(1)
+  .insert(45)
+  .insert(199);
+console.log(heap);
+
+console.log(heap.extractMax());
+console.log(heap.values);
+
+console.log(heap.extractMax());
+console.log(heap.values);
+
+console.log(heap.extractMax());
+console.log(heap.values);
+
+console.log(heap.extractMax());
+console.log(heap.values);
