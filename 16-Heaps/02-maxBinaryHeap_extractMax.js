fb602eb05dd732b8f91a999ef560772ee0e53831
Dhruv Arora <dhruvarora.code@gmail.com>
2023-07-07 18:06:47 +0530

    extract max implemented

--- 16-Heaps/02-maxBinaryHeap_extractMax.js
+++ 16-Heaps/02-maxBinaryHeap_extractMax.js
@@ -22,30 +22,46 @@ class MaxBinaryHeap {
 
   //remove or extract max:
   extractMax() {
-    let arrLength = this.values.length;
+    const max = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return max;
+  }
 
-    if (!arrLength) {
-      return false;
-    } else if (arrLength === 1) {
-      return this.values.pop();
-    } else {
-      let last = this.values.length - 1;
+  sinkDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
 
-      let temp = this.values[last];
-      this.values[last] = this.values[0];
-      this.values[0] = temp;
-      let removed = this.values.pop();
-
-      let currentIdx = 0;
-      let current = this.values[currentIdx];
-      let leftIdx = 2 * currentIdx + 1;
-      let rightIdx = 2 * currentIdx + 2;
-
-      if (this.values[leftIdx] && this.values[rightIdx]) {
-        if (this.values[leftIdx] < current && this.values[rightIdx] < current) {
-          return removed;
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+        if (leftChild > element) {
+          swap = leftChildIdx;
         }
       }
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+        if (
+          (swap === null && rightChild > element) ||
+          (swap !== null && rightChild > leftChild)
+        ) {
+          swap = rightChildIdx;
+        }
+      }
+
+      if (swap === null) break;
+      let temp = this.values[idx];
+      this.values[idx] = this.values[swap];
+      this.values[swap] = temp;
+      idx = swap;
     }
   }
 }
@@ -82,3 +98,15 @@ heap
   .insert(45)
   .insert(199);
 console.log(heap);
+
+console.log(heap.extractMax());
+console.log(heap.values);
+
+console.log(heap.extractMax());
+console.log(heap.values);
+
+console.log(heap.extractMax());
+console.log(heap.values);
+
+console.log(heap.extractMax());
+console.log(heap.values);
