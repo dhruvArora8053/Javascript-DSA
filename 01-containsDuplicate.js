4bea448014931fafa1d61be37e8995c4558bfb57
Dhruv Arora <dhruvarora.code@gmail.com>
2023-07-11 15:52:04 +0530

    containes duplicate implemented

--- 01-containsDuplicate.js
+++ 01-containsDuplicate.js
@@ -1,3 +1,22 @@
 "use strict";
 
 //https://leetcode.com/problems/contains-duplicate/
+
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+const containsDuplicate = function (arr) {
+  let containes = false;
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] === arr[j]) {
+        containes = true;
+      }
+    }
+  }
+  return containes;
+};
+
+console.log(containsDuplicate([1, 2, 3, 1]));
+console.log(containsDuplicate([1, 2, 3, 4]));
+console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]));
