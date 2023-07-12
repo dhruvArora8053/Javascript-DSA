b0a1b0e5bbd5fd766aee31acf9babc4f8e68efdc
Dhruv Arora <dhruvarora.code@gmail.com>
2023-07-12 22:12:07 +0530

    added edges

--- 19-Graphs/02-addEdges.js
+++ 19-Graphs/02-addEdges.js
@@ -8,10 +8,18 @@ class Graph {
   addVertex(vertex) {
     if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
   }
+
+  addEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1].push(vertex2);
+    this.adjacencyList[vertex2].push(vertex1);
+  }
 }
 
 const g = new Graph();
 g.addVertex("Tokyo");
 g.addVertex("Dallas");
 g.addVertex("Aspen");
+
+g.addEdge("Tokyo", "Dallas");
+g.addEdge("Dallas", "Aspen");
 console.log(g);
