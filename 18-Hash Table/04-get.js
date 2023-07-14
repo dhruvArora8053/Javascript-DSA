f8b6b8e5136a62a9bf7ca790761e40aaacfced1a
Dhruv Arora <dhruvarora.code@gmail.com>
2023-07-15 00:15:10 +0530

    keys and values implemented

---
+++ 18-Hash Table/04-get.js
@@ -0,0 +1,70 @@
+"use strict";
+
+//simple hash function:
+class HashTable {
+  constructor(size = 4) {
+    this.keyMap = new Array(size);
+  }
+
+  _hash(key) {
+    let total = 0;
+    let WEIRD_PRIME = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0) - 96;
+      total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+    }
+    return total;
+  }
+
+  set(key, value) {
+    let index = this._hash(key);
+
+    if (!this.keyMap[index]) {
+      this.keyMap[index] = [];
+    }
+    this.keyMap[index].push([key, value]);
+    return index;
+  }
+
+  //   get(key) {
+  //     let index = this._hash(key);
+  //     const pair = this.keyMap[index].filter((pair) => {
+  //       pair[0] === key;
+  //       return pair[1];
+  //     });
+
+  //     if (pair) {
+  //       return pair;
+  //     }
+  //     return null;
+  //   }
+
+  //steele's solution
+  get(key) {
+    let index = this._hash(key);
+    if (this.keyMap[index]) {
+      for (let i = 0; i < this.keyMap[index].length; i++) {
+        if (this.keyMap[index][i][0] === key) {
+          return this.keyMap[index][i][1];
+        }
+      }
+    }
+    return undefined;
+  }
+}
+
+let ht = new HashTable();
+console.log(ht.set("hello world", "goodbye!!"));
+console.log(ht.set("dogs", "are cool"));
+console.log(ht.set("cats", "are fine"));
+console.log(ht.set("i love", "pizza"));
+console.log(ht.set("hi", "bye"));
+console.log(ht.set("french", "fries"));
+console.log(ht);
+
+console.log(ht.get("french"));
+console.log(ht.get("i love"));
+console.log(ht.get("dogs"));
+console.log(ht.get("cats"));
+console.log(ht.get("let's do it"));
---
+++ 18-Hash Table/05-key&Values.js
@@ -0,0 +1,87 @@
+"use strict";
+
+//simple hash function:
+class HashTable {
+  constructor(size = 4) {
+    this.keyMap = new Array(size);
+  }
+
+  _hash(key) {
+    let total = 0;
+    let WEIRD_PRIME = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0) - 96;
+      total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+    }
+    return total;
+  }
+
+  set(key, value) {
+    let index = this._hash(key);
+
+    if (!this.keyMap[index]) {
+      this.keyMap[index] = [];
+    }
+    this.keyMap[index].push([key, value]);
+    return index;
+  }
+
+  //   get(key) {
+  //     let index = this._hash(key);
+  //     const pair = this.keyMap[index].filter((pair) => {
+  //       pair[0] === key;
+  //       return pair[1];
+  //     });
+
+  //     if (pair) {
+  //       return pair;
+  //     }
+  //     return null;
+  //   }
+
+  //steele's solution
+  get(key) {
+    let index = this._hash(key);
+    if (this.keyMap[index]) {
+      for (let i = 0; i < this.keyMap[index].length; i++) {
+        if (this.keyMap[index][i][0] === key) {
+          return this.keyMap[index][i][1];
+        }
+      }
+    }
+    return undefined;
+  }
+
+  keys() {
+    const keys = [];
+    for (let i = 0; i < this.keyMap.length; i++) {
+      for (let j = 0; j < this.keyMap[i].length; j++) {
+        keys.push(this.keyMap[i][j][0]);
+      }
+    }
+    return keys;
+  }
+
+  values() {
+    const keys = [];
+    for (let i = 0; i < this.keyMap.length; i++) {
+      for (let j = 0; j < this.keyMap[i].length; j++) {
+        keys.push(this.keyMap[i][j][1]);
+      }
+    }
+    return keys;
+  }
+}
+
+let ht = new HashTable();
+console.log(ht.set("hello world", "goodbye!!"));
+console.log(ht.set("dogs", "are cool"));
+console.log(ht.set("cats", "are fine"));
+console.log(ht.set("i love", "pizza"));
+console.log(ht.set("hi", "bye"));
+console.log(ht.set("french", "fries"));
+console.log(ht);
+
+console.log(ht.keys());
+console.log(ht.values());
