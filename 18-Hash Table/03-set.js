af185a2499ecff88e291882c539aa931a404e75d
Dhruv Arora <dhruvarora.code@gmail.com>
2023-07-14 23:51:24 +0530

    implementing set

--- 18-Hash Table/03-set.js
+++ 18-Hash Table/03-set.js
@@ -1,27 +1,32 @@
 "use strict";
 
 //simple hash function:
-const hash = function (key, arrayLen) {
-  let total = 0;
-  let wierdPrime = 31;
-
-  for (let i = 0; i < Math.min(key.length, 100); i++) {
-    let char = key[i];
-    let value = char.charCodeAt(0) - 96;
-    total = (total * wierdPrime + value) % arrayLen;
+class HashTable {
+  constructor(size = 53) {
+    this.keyMap = new Array(size);
   }
-  return total;
-};
 
-console.log(hash("pink", 13));
-console.log(hash("pink", 13));
-console.log(hash("pink", 13));
-console.log(hash("pink", 13));
-console.log(hash("cyan", 13));
-console.log(hash("green", 13));
-console.log(hash("orange", 13));
-console.log(hash("purple", 13));
-console.log(hash("maroon", 13));
-//problems with this func:- only hashes strings
-//2. not constant time, as the string grows, loop grows
-//3. not much randomness, our data can be clustered very easily
+  _hash(key) {
+    let total = 0;
+    let WEIRD_PRIME = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0) - 96;
+      total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+    }
+    return total;
+  }
+
+  set(key, value) {
+    let index = this._hash(key);
+
+    if (!this.keyMap[index]) {
+      this.keyMap[index] = [];
+    }
+    this.keyMap[index].push([key, value]);
+    return index;
+  }
+}
+
+let ht = new HashTable();
+console.log(ht.set("hello world", "goodbye!!"));
