4c3c2e0f3d5c1c2a1fa596c8065a43a78c5f5852
Dhruv Arora <dhruvarora.code@gmail.com>
2023-07-15 10:27:14 +0530

    implementing validAnagram

--- 02-validAnagram.js
+++ 02-validAnagram.js
@@ -2,6 +2,7 @@
 
 //https://leetcode.com/problems/valid-anagram/
 
+//My Solution
 const validAnagram = function (s, t) {
   const sArr = s.split("");
   const tArr = t.split("");
@@ -18,5 +19,32 @@ const validAnagram = function (s, t) {
 };
 
 console.log(validAnagram("anagram", "nagaram"));
+console.log(validAnagram("rat", "car"));
+console.log(validAnagram("aacc", "ccac"));
+console.log("****************************");
+
+//2nd Solution:
+const validAnagram1 = function (s, t) {
+  const sArr = s.split("");
+  const tArr = t.split("");
+  const sChars = [];
+  const tChars = [];
+  let sTotal = 0;
+  let tTotal = 0;
+
+  if (sArr.length !== tArr.length) return false;
+
+  for (let i = 0; i < sArr.length; i++) {
+    sTotal += sArr[i].charCodeAt();
+  }
+  for (let i = 0; i < tArr.length; i++) {
+    tTotal += tArr[i].charCodeAt();
+  }
+
+  if (sTotal === tTotal) return true;
+  else return false;
+};
+
+console.log(validAnagram1("anagram", "nagaram"));
 console.log(validAnagram("rat", "car"));
 console.log(validAnagram("aacc", "ccac"));
