48c9c1dc864c805dd3b88b7f15bda570ae3f2d54
Dhruv Arora <dhruvarora.code@gmail.com>
2024-02-07 14:59:21 +0530

    implementing insertion sort

--- 07-Insertion Sort/insertionSort.js
+++ 07-Insertion Sort/insertionSort.js
@@ -26,13 +26,8 @@ console.log(insertionSort([-8, 1, 2, -3, 4, -5, 6, 7]));
 
 const insertionSort1 = function (arr) {
   for (let i = 1; i < arr.length; i++) {
-    let currentVal = arr[i];
-    for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
-      arr[j + 1] = arr[j];
-    }
-    arr[j + 1] = currentVal;
+    for (let j = i - 1; j >= 0; j--) {}
   }
-  return arr;
 };
 
 console.log(insertionSort1([2, 1, 9, 76, 4]));
